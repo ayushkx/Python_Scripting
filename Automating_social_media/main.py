36b37684ea500038d631a76c9add205a78fc36d6
ayushkx <ayushkushwaha855@gmail.com>
2024-06-04 23:47:57 +0530

    update

--- Automating_social_media/main.py
+++ Automating_social_media/main.py
@@ -1,19 +1,20 @@
 #importing selenium module to handle browser activity , getpass to get secure password and time module to delay
 import os
 
-try:
-    from getpass import getpass
-    from selenium import webdriver
-    from time import sleep
-    import webdriver_manager
-    from webdriver_manager.chrome import ChromeDriverManager
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.common.by import By
-except ModuleNotFoundError:
-    os.system('pip install -r requirements.txt')
-    os.system('python -m pip install --upgrade pip')
-    print("\n\n\n\nPlease Restart this Software\n\n\n\nThanks for your Co-operation")
-    exit()
+# try:
+from getpass import getpass
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from time import sleep
+import webdriver_manager
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By\
+# except ModuleNotFoundError:
+#     #os.system('pip install -r requirements.txt')
+#     os.system('python -m pip install --upgrade pip')
+#     print("\n\n\n\nPlease Restart this Software\n\n\n\nThanks for your Co-operation")
+#     exit()
 
 #taking user credentials
 user = input("username")
@@ -28,7 +29,7 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
 #initiate the chrome driver and open the url
-browser = webdriver.Chrome(ChromeDriverManager().install() , options=chrome_options)
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
 browser.get('www.instagram.com')
 print("opened insta")
 sleep(1)
@@ -49,34 +50,34 @@ login_box.click()
 print("Login successfully..")
 
 
-def uploadToInstagram(Image , description,username):
-    browser.find_element(By.XPATH, "/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button/div").click()
-    sleep(5)
-    browser.get(f'https://instagram.com/{username}')
-    sleep(5)
-    browser.find_element(By.CSS_SELECTOR, '[aria-label="New post"]').click()
-    browser.find_element(By.XPATH, '//*[@id="mount_0_0_AM"]/div/div[1]/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div/div/div/div/div[2]/div[1]/form/input').send_keys(Image)
+# def uploadToInstagram(Image , description,username):
+#     browser.find_element(By.XPATH, "/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]/button/div").click()
+#     sleep(5)
+#     browser.get(f'https://instagram.com/{username}')
+#     sleep(5)
+#     browser.find_element(By.CSS_SELECTOR, '[aria-label="New post"]').click()
+#     browser.find_element(By.XPATH, '//*[@id="mount_0_0_AM"]/div/div[1]/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div/div/div/div/div[2]/div[1]/form/input').send_keys(Image)
 
-    sleep(200)
+#     sleep(200)
 
-    browser.find_element(By.XPATH, "/html/body/div[6]/div[2]/div/div/div/div[1]/div/div/div[3]/div/button").click()
-    sleep(2)
-    browser.find_element(By.XPATH, "/html/body/div[6]/div[2]/div/div/div/div[1]/div/div/div[3]/div/button").click()
-    sleep(2)
-    browser.find_element(By.XPATH, "/html/body/div[6]/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[2]/div[1]/textarea").send_keys(description)
-    sleep(2)
-    browser.find_element(By.XPATH, "/html/body/div[6]/div[2]/div/div/div/div[1]/div/div/div[3]/div/button").click()
-    sleep(2)
+#     browser.find_element(By.XPATH, "/html/body/div[6]/div[2]/div/div/div/div[1]/div/div/div[3]/div/button").click()
+#     sleep(2)
+#     browser.find_element(By.XPATH, "/html/body/div[6]/div[2]/div/div/div/div[1]/div/div/div[3]/div/button").click()
+#     sleep(2)
+#     browser.find_element(By.XPATH, "/html/body/div[6]/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[2]/div[1]/textarea").send_keys(description)
+#     sleep(2)
+#     browser.find_element(By.XPATH, "/html/body/div[6]/div[2]/div/div/div/div[1]/div/div/div[3]/div/button").click()
+#     sleep(2)
 
-    ImageUploaded = False
-    while ImageUploaded == False:
-        try:
-            myElem = browser.find_element(By.XPATH, "/html/body/div[6]/div[2]/div/div/div/div[2]/div[1]/div/img").click()
-            ImageUploaded = True
-        except:
-            sleep(5)
-            ImageUploaded = False
-    sleep(5)
+#     ImageUploaded = False
+#     while ImageUploaded == False:
+#         try:
+#             myElem = browser.find_element(By.XPATH, "/html/body/div[6]/div[2]/div/div/div/div[2]/div[1]/div/img").click()
+#             ImageUploaded = True
+#         except:
+#             sleep(5)
+#             ImageUploaded = False
+#     sleep(5)
 
 #exit the session using quit()
 exitSession = input("Enter q to quit session: ")
